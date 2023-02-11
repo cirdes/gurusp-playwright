c520ed0b311756059c16fdaccbb5e981a3a9b1d4
Kai Breitbarth <kaibreitbarth@Kais-MacBook-Air.fritz.box>
2023-02-11 12:26:01 +0100

    cleaned up

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -3,13 +3,26 @@ import { Layout, Menu } from "antd";
 
 const { Header } = Layout;
 
+const menuItems = [
+    {
+        key: "1",
+        label: "Home",
+    },
+    {
+        key: "2",
+        label: "Our Services",
+    },
+    {
+        key: "3",
+        label: "Contact",
+    },
+]
+
 export default () => (
   <Header>
     <div className="logo" />
-    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-      <Menu.Item key="1">Home</Menu.Item>
-      <Menu.Item key="2">Our Services</Menu.Item>
-      <Menu.Item key="3">Contact</Menu.Item>
+    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]} items={menuItems}>
+      
     </Menu>
   </Header>
 );
