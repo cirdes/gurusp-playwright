314e38b9170da0b31acf32bc63ce2cd4c5d5af13
Kai Breitbarth <kai.breitbarth@gmail.com>
2023-02-13 19:10:39 +0100

    updated rails

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,8 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import ReactController from "./react_controller"
 application.register("react", ReactController)
