383b02ee8b20e77a029baeb1eac283db224db6d1
Breno Moura <basmoura@gmail.com>
2023-07-26 21:19:34 -0300

    reactor: add show path to react routes

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
+import Show from "../components/Show";
 
 export default (
   <Router>
     <Routes>
-      <Route path="/" element={<Home/>} />   
+      <Route path="/" element={<Home/>} />
+      <Route exact path="/beers/:id" element={<Show/>} />
     </Routes>
   </Router>
 );
